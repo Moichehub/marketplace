14bd8a90f398e84a025d0f5d3c691c38d75505b3
Your Name <egor@gmail.com>
2025-08-26 20:01:30 +0300

    Fix dependecy control

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -12,12 +12,12 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
-from dotenv import load_dotenv
+from decouple import config
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-change-me-in-production')
 
 DEBUG = True
 
